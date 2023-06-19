ffbbb3edf644ad91591f269e5bd4b9e8708adad1
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-06-19 15:59:38 +0700

    Fix z value for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Header() {
   return (
     <header
-      className="h-24 sticky top-0 text-white text-xl py-8 px-4 bg-[#121212]"
+      className="h-24 sticky top-0 text-white text-xl py-8 px-4 bg-[#121212] z-10"
       id="header"
     >
       <nav className="h-full flex items-center justify-between">
