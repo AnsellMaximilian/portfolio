0151b09cabb1a650f7c2b79257e997045cab0abd
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-10-10 09:29:18 +0700

    Update: hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { BsFillTelephoneFill, BsTelephoneFill } from "react-icons/bs";
 
 export default function Hero() {
   return (
@@ -25,18 +26,20 @@ export default function Hero() {
       </div>
       <div>
         <p className="mb-4">
-          A computer science graduate majoring in information systems. I&apos;ve
-          self-taught to code both front end and backend. My strength lies in
-          crafting front-end solutions, which I&apos;ve applied professionally.
-          I focus on creating user-centered interfaces and enjoy using React and
-          Tailwind for the job.
+          A computer science graduate majoring in information systems. My
+          strength lies in crafting front-end solutions, which I&apos;ve applied
+          professionally. I focus on creating user-centered interfaces and enjoy
+          using React and Tailwind for the job. I also love participating in
+          hackathons and trying to develop creative solutions with new
+          technologies.
         </p>
         <div className="flex gap-4 items-center flex-wrap">
           <a
             href="#contact-section"
-            className="bg-white text-black hover:bg-gray-300 border-2 border-white px-4 py-2 font-bold hover:border-gray-300 transition-all duration-100"
+            className="flex gap-2 items-center bg-white text-black hover:bg-gray-300 border-2 border-white px-4 py-2 font-bold hover:border-gray-300 transition-all duration-100"
           >
-            Contact Me
+            <BsTelephoneFill />
+            <span>Contact Me</span>
           </a>
           <Link
             href="/Ansell_Maximilian_Resume.pdf"
@@ -44,12 +47,6 @@ export default function Hero() {
           >
             Resume
           </Link>
-          {/* <a
-            href="#projects"
-            className="hover:bg-gray-300 border-2 border-white px-4 py-2 font-bold hover:border-gray-300 transition-all duration-100 hover:text-black"
-          >
-            Projects
-          </a> */}
         </div>
       </div>
     </motion.div>
