45107f542a04af22ada29ec089cf44074576b842
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-11-09 13:11:53 +0700

    make hackathons responsive

--- components/HackathonList.tsx
+++ components/HackathonList.tsx
@@ -10,26 +10,29 @@ export default async function HackathonList() {
       {hackathons.map((hack) => (
         <div
           key={hack._id}
-          className="p-4 py-6 border-zinc-200 rounded-md border shadow-md col-span-12 sm:col-span-6 lg:col-span-4"
+          className="p-4 py-6 border-zinc-200 rounded-md border shadow-md col-span-12 sm:col-span-6 lg:col-span-4 flex flex-col gap-8"
         >
-          <div className="flex gap-2 items-start">
-            <Trophy className="text-[4rem] text-yellow-500" />
-            <div>
-              <h3 className="text-2xl font-bold">{hack.name}</h3>
-              <p className="font-semibold text-lg">{hack.placement}</p>
+          <div>
+            <div className="flex gap-2 items-start">
+              <div>
+                <Trophy className="text-5xl lg:text-[4rem] text-yellow-500" />
+              </div>
+              <div>
+                <h3 className="text-xl lg:text-2xl font-bold">{hack.name}</h3>
+                <p className="font-semibold lg:text-lg">{hack.placement}</p>
+              </div>
             </div>
+            <p className="mt-4">
+              <span className="font-bold">Project:</span>{" "}
+              <Link
+                href={`/projects/${hack.project.slug}`}
+                className="hover:underline"
+              >
+                {hack.project.name}
+              </Link>
+            </p>
           </div>
-          <p className="font-light mt-4">{hack.description}</p>
-          <p className="mt-4">
-            <span className="font-bold">Project:</span>{" "}
-            <Link
-              href={`/projects/${hack.project.slug}`}
-              className="hover:underline"
-            >
-              {hack.project.name}
-            </Link>
-          </p>
-          <div className="mt-8 flex gap-2 justify-end ">
+          <div className="flex gap-2 justify-end mt-auto">
             <Link
               href={`/projects/${hack.project.slug}`}
               className="border-[#121212] border px-4 py-2 hover:bg-zinc-100"
